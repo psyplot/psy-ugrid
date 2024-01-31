4b3a7288e22c3cacf5a37b7254952a98234bbf3e
Philipp Sommer <philipp.sommer@hereon.de>
2024-01-31 13:22:18 +0100

    build the c-code when unavailable

--- setup.py
+++ setup.py
@@ -9,8 +9,13 @@ import versioneer
 from setuptools import Extension, setup
 
 _USE_CYTHON = os.getenv("USE_CYTHON", "").lower()
-USE_CYTHON = bool(_USE_CYTHON) and (
-    not _USE_CYTHON.startswith("f") and not _USE_CYTHON.startswith("n")
+# use cython, when there is no c code or when it is specified via environment
+# variable
+USE_CYTHON = not os.path.exists(
+    os.path.join("psy_ugrid", "_create_dual_node_mesh.c")
+) or (
+    bool(_USE_CYTHON)
+    and (not _USE_CYTHON.startswith("f") and not _USE_CYTHON.startswith("n"))
 )
 
 ext = ".pyx" if USE_CYTHON else ".c"
